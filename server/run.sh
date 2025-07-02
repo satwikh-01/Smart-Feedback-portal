68376992f353d57a359a55fa52357bb8de0586fd
satwikh-01 <satwikh_01@icloud.com>
2025-07-02 10:48:44 +0530

    Build configuration modified

--- server/run.sh
+++ server/run.sh
@@ -5,4 +5,4 @@ set -e
 PORT=${PORT:-8000}
 
 # Start uvicorn
-exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT"
+exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT" --forwarded-allow-ips='*'
